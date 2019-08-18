b61c49ec4b4b90f805936d32c44e4554bd18b130
Happy_Baozi <happy_bz@qq.com>
2019-08-18 10:46:12 +0800

    买家端商品

---
+++ src/main/java/com/happybz/sell/repository/ProductInfoRepository.java
@@ -0,0 +1,11 @@
+package com.happybz.sell.repository;
+
+import com.happybz.sell.dataobject.ProductInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductInfoRepository extends JpaRepository<ProductInfo, String> {
+
+    List<ProductInfo> findByProductStatus(Integer productStatus);
+}
---
+++ src/main/java/com/happybz/sell/service/impl/ProductServiceImpl.java
@@ -0,0 +1,39 @@
+package com.happybz.sell.service.impl;
+
+import com.happybz.sell.dataobject.ProductInfo;
+import com.happybz.sell.enums.ProductStatusEnum;
+import com.happybz.sell.repository.ProductInfoRepository;
+import com.happybz.sell.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductInfoRepository repository;
+
+    @Override
+    public ProductInfo findOne(String productId) {
+        return repository.findById(productId).orElse(null);
+    }
+
+    @Override
+    public List<ProductInfo> findUpAll() {
+        return repository.findByProductStatus(ProductStatusEnum.UP.getCode());
+    }
+
+    @Override
+    public Page<ProductInfo> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public ProductInfo save(ProductInfo productInfo) {
+        return repository.save(productInfo);
+    }
+}
